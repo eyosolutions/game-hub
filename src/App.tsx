a09f650109eabe492f4552926fe13d467ff369dc
eyosolutions <emmanuel.y.odoom@gmail.com>
2023-12-05 00:03:35 +0000

    Implement responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="md">
+        <GridItem area="aside" bg="magenta">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="teal">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
